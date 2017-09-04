b5adc17edbe04c902905d6cd97f65f2c1607f9ed
zhaoyu02 <zhaoyu02@ppdai.com>
2017-09-04 20:34:53 +0800

    Project Config

--- src/cn/aofeng/threadpool4j/ThreadPoolManager.java
+++ src/cn/aofeng/threadpool4j/ThreadPoolManager.java
@@ -4,18 +4,19 @@ import cn.aofeng.common4j.ILifeCycle;
 
 /**
  * 线程池实例管理。
- * 
+ *
  * @author <a href="mailto:aofengblog@163.com">聂勇</a>
  */
 public class ThreadPoolManager implements ILifeCycle {
 
-    private ILifeCycle _threadPool = new ThreadPoolImpl(); 
-    
+    private ILifeCycle _threadPool = new ThreadPoolImpl();
+
     private static Object _lock = new Object();
     private boolean _initStatus = false;
     private boolean _destroyStatus = false;
-    
+
     private static ThreadPoolManager _instance = new ThreadPoolManager();
+
     public static ThreadPoolManager getSingleton() {
         return _instance;
     }
@@ -23,12 +24,12 @@ public class ThreadPoolManager implements ILifeCycle {
     public ThreadPool getThreadPool() {
         return (ThreadPool) _threadPool;
     }
-    
+
     // 用于单元测试和子类扩展
     protected void setThreadPool(ThreadPool threadPool) {
         this._threadPool = (ILifeCycle) threadPool;
     }
-    
+
     @Override
     public void init() {
         synchronized (_lock) {

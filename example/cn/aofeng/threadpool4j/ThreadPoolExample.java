177525bde2f021860bf88c05e520388df255fe03
zhaoyu <907219202@qq.com>
2017-09-10 17:22:59 +0800

    update

--- example/cn/aofeng/threadpool4j/ThreadPoolExample.java
+++ example/cn/aofeng/threadpool4j/ThreadPoolExample.java
@@ -9,20 +9,20 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 多线程池调用代码示例。
- * 
+ *
  * @author <a href="mailto:aofengblog@163.com">聂勇</a>
  */
 public class ThreadPoolExample {
-    
+
     public static void main(String[] args) throws Exception {
         ThreadPoolManager tpm = ThreadPoolManager.getSingleton();
         ThreadPool threadPool = tpm.getThreadPool();
         tpm.init(); // 在应用启动时调用
-        
+
         executeRunnableAnsyTask(threadPool);
         executeCallableAnsyTask(threadPool);
         parallelExecuteAnsyTask(threadPool);
-        
+
         tpm.destroy(); // 在应用关闭时调用
     }
 
@@ -34,11 +34,11 @@ public class ThreadPoolExample {
         for (int i = 0; i < 100; i++) {
             threadPool.submit(new RunnableAsynTask());
             threadPool.submit(new RunnableAsynTask(), "other");
-            
+
             Thread.sleep(50);
         }
     }
-    
+
     /**
      * 执行需要返回值的异步任务。
      */
@@ -55,20 +55,20 @@ public class ThreadPoolExample {
     /**
      * 并行调用多个异步任务。
      */
-    private static void parallelExecuteAnsyTask(ThreadPool threadPool) 
+    private static void parallelExecuteAnsyTask(ThreadPool threadPool)
             throws InterruptedException, ExecutionException {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
         tasks.add(new CallableAnsyTask(arr));
         tasks.add(new CallableAnsyTask(arr));
         tasks.add(new CallableAnsyTask(arr));
-        
+
         List<Future<Long>> futures = threadPool.invokeAll(tasks, 1, TimeUnit.SECONDS);
         for (Future<Long> future : futures) {
             Long result = future.get();   // 如果某个任务执行超时，调用该任务对应的future.get时抛出CancellationException异常
             System.out.println("并行调用，其中一个任务的执行结果为:"+result);
         }
-        
+
     }
 
 }
